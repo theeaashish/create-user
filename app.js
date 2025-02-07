91947326d870b0fc881413e7d44e627da0a3be65
Aashish Joshi <110759864+theeaashish@users.noreply.github.com>
2025-02-07 23:38:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+const path = require("path");
+const userModel = require("./models/user");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/read", async (req, res) => {
+  let users = await userModel.find();
+  res.render("read", { users });
+});
+
+app.post("/create", async (req, res) => {
+  let { name, email, image } = req.body;
+
+  let createdUser = await userModel.create({ name, email, image });
+  res.redirect("/read");
+});
+
+app.get("/delete/:id", async (req, res) => {
+  let deletedUser = await userModel.findByIdAndDelete(req.params.id);
+  res.redirect("/read");
+});
+
+app.listen(3000);
