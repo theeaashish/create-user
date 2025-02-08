62af566aecede90eb1a65dc1ea48b7a64f28caa6
Aashish Joshi <110759864+theeaashish@users.noreply.github.com>
2025-02-08 14:28:49 +0530

    update user functionality is added

--- app.js
+++ app.js
@@ -30,4 +30,15 @@ app.get("/delete/:id", async (req, res) => {
   res.redirect("/read");
 });
 
+app.get("/edit/:userid", async (req, res) => {
+    let user = await userModel.findOne({_id: req.params.userid});
+    res.render("edit", {user});
+})
+
+app.post("/update/:userid", async (req, res) => {
+    let {name, email, image} = req.body;
+    let user = await userModel.findOneAndUpdate({_id: req.params.userid}, {name, email, image}, {new: true}); 
+    res.redirect("/read");
+})
+
 app.listen(3000);
